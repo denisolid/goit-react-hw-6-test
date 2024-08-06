35c8f36584ec66c8af399d2b8ff0bdb47e828ea5
denisolid <dkolisnichienko321@gmail.com>
2024-08-06 20:08:51 +0000

    app jsx updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,9 @@
-import { useSelector } from "react-redux";
+import { Counter } from "./Counter/Counter";
 
-const App = () => {
-  const counter = useSelector((state) => state.counter.counter);
-  const step = useSelector((state) => state.counter.step);
-
+export const App = () => {
   return (
-    <div className={s.flexContainer}>
-      <div className={s.wrapper}>
-        <h1>{counter}</h1>
-        <input value={step} onChange={handleChangeStep} />
-        <div className={s.flex}>
-          <button className="btn" onClick={handleMinusClick}>
-            minus
-          </button>
-          <button className="btn" onClick={handleResetClick}>
-            reset
-          </button>
-          <button className="btn" onClick={handlePlusClick}>
-            plus
-          </button>
-        </div>
-      </div>
+    <div>
+      <Counter />
     </div>
   );
 };
