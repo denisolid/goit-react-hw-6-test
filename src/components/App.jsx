a6fdeeff102ca9179e089d4edca6a4492596db03
denisolid <dkolisnichienko321@gmail.com>
2024-08-06 19:22:09 +0000

    useSelector for part of state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,91 +1,26 @@
-import { useEffect, useState } from "react";
-import ImageGallery from "./ImageGallery/ImageGallery";
-import { searchImages } from "../services/api";
-import SearchBar from "./SearchBar/SearchBar";
-import Loader from "./Loader/Loader";
-import ErrorMessage from "./ErrorMessage/ErrorMessage";
-import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
-import ImageModal from "./ImageModal/ImageModal";
-import Modal from "react-modal";
-import s from "./App.module.css";
-
-Modal.setAppElement("#root");
+import { useSelector } from "react-redux";
 
 const App = () => {
-  const [images, setImages] = useState([]);
-  const [query, setQuery] = useState("");
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [noResults, setNoResults] = useState(false); // Додана нова змінна стану
-
-  useEffect(() => {
-    if (!query) return;
-
-    const fetchImages = async () => {
-      setIsLoading(true);
-      try {
-        const data = await searchImages(query, page);
-        if (data.results.length === 0 && page === 1) {
-          setNoResults(true);
-        } else {
-          setNoResults(false);
-        }
-        setImages((prevImages) => [...prevImages, ...data.results]);
-      } catch (err) {
-        setError("Failed to fetch images");
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchImages();
-  }, [query, page]);
-
-  const handleSearchSubmit = (newQuery) => {
-    setQuery(newQuery);
-    setPage(1);
-    setImages([]);
-    setNoResults(false);
-  };
-
-  const handleLoadMore = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
-
-  const openModal = (description, imageUrl) => {
-    setSelectedImage({ description, imageUrl });
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedImage(null);
-  };
+  const counter = useSelector((state) => state.counter.counter);
+  const step = useSelector((state) => state.counter.step);
 
   return (
-    <div>
-      <SearchBar setQuery={handleSearchSubmit} />
-      {error && <ErrorMessage message={error} />}
-      {noResults && (
-        <div className={s.notification}>
-          No images found for your search query
+    <div className={s.flexContainer}>
+      <div className={s.wrapper}>
+        <h1>{counter}</h1>
+        <input value={step} onChange={handleChangeStep} />
+        <div className={s.flex}>
+          <button className="btn" onClick={handleMinusClick}>
+            minus
+          </button>
+          <button className="btn" onClick={handleResetClick}>
+            reset
+          </button>
+          <button className="btn" onClick={handlePlusClick}>
+            plus
+          </button>
         </div>
-      )}
-      <ImageGallery images={images} openModal={openModal} />
-      {isLoading && <Loader />}
-      {images.length > 0 && !isLoading && !noResults && (
-        <LoadMoreBtn onClick={handleLoadMore} />
-      )}
-      {selectedImage && (
-        <ImageModal
-          isOpen={isModalOpen}
-          onRequestClose={closeModal}
-          image={selectedImage}
-        />
-      )}
+      </div>
     </div>
   );
 };
