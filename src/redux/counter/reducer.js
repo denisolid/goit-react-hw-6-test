d844490231df93decfef7b74710b4a0ec3956e8d
denisolid <dkolisnichienko321@gmail.com>
2024-08-07 19:15:32 +0000

    create reducer

--- src/redux/counter/reducer.js
+++ src/redux/counter/reducer.js
@@ -1,22 +1,38 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { changeStep, decrement, increment, reset } from "./actions";
-import { CHANGE_STEP, DECREMENT, INCREMENT, RESET } from "./constants";
+// import { CHANGE_STEP, DECREMENT, INCREMENT, RESET } from "./constants";
 
 const initialState = {
   counter: 0,
   step: 1,
 };
 
-export const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case increment.type:
-      return { ...state, counter: state.counter + state.step };
-    case decrement.type:
-      return { ...state, counter: state.counter - state.step };
-    case reset.type:
-      return { ...state, counter: 0, step: 1 };
-    case changeStep.type:
-      return { ...state, step: action.payload };
-    default:
-      return state;
-  }
-};
+export const counterReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(increment, (state, action) => {
+      state.counter += state.step;
+    })
+    .addCase(increment, (state, action) => {
+      state.counter -= state.step;
+    })
+    .addCase(reset, (state, action) => {
+      return initialState;
+    })
+    .addCase(changeStep, (state, action) => {
+      state.step -= action.payload;
+    });
+});
+// export const counterReducer = (state = initialState, acti=on) => {
+//   switch (action.type) {
+//     case increment.type:
+//       return { ...state, counter: state.counter + state.step };
+//     case decrement.type:
+//       return { ...state, counter: state.counter - state.step };
+//     case reset.type:
+//       return { ...state, counter: 0, step: 1 };
+//     case changeStep.type:
+//       return { ...state, step: action.payload };
+//     default:
+//       return state;
+//   }
+// };
